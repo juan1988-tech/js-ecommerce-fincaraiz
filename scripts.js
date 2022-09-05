d0c56331d47b6ddfce0ec198f489dd054b0b449f
Juan Franco <jfrancoh5@gmail.com>
2022-09-05 14:18:33 -0500

    web components de casas listo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-const Data = [
+ const Data = [
   { 
-    urlImage: 'url(./assets/homes/casa-1-1.jpg)',
+    urlImage1: 'url(./assets/homes/casa-1-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-1-2.jpg)',
+    urlImage3: 'url(./assets/homes/casa-1-3.jpg)',
     stateHomeText: 'En Arrendamiento',
     HomeTitleText:  'Elíxires del rincón',
     HomeSubTitleText: 'Suba, Bogotá',
@@ -10,7 +12,9 @@ const Data = [
     PropertyBathroomsPText: "3 baños"
   },
   {
-    urlImage: 'url(./assets/homes/casa-2-1.jpg)',
+    urlImage1: 'url(./assets/homes/casa-2-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-2-2.jpg)',
+    urlImage3: 'url(./assets/homes/casa-2-3.jpg)',
     stateHomeText: 'En Venta',
     HomeTitleText:  ' Cañiza de la olla',
     HomeSubTitleText: 'Centro, Bogotá',
@@ -20,7 +24,9 @@ const Data = [
     PropertyBathroomsPText: "3 baños"
   },
   {
-    urlImage: 'url(./assets/homes/casa-3-1.jpg)',
+    urlImage1: 'url(./assets/homes/casa-3-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-3-2.jpg)',
+    urlImage3: 'url(./assets/homes/casa-3-3.jpg)',
     stateHomeText: 'En Arrendamiento',
     HomeTitleText:  'Altos del sumapáz',
     HomeSubTitleText: 'Alamos, Bogotá',
@@ -29,97 +35,140 @@ const Data = [
     PropertyMeasurePText: "300 m2",
     PropertyBathroomsPText: "2 baños"
   },
+  {
+    urlImage1: 'url(./assets/homes/casa-4-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-4-2.jpg)', 
+    urlImage3: 'url(./assets/homes/casa-4-3.jpg)',
+    stateHomeText: 'En Venta',
+    HomeTitleText:  'Altos de la chicha',
+    HomeSubTitleText: 'La palma, Bogotá',
+    PropertyNamePText:'Altos de la conejera',
+    PropertyBedroomsPText: "3 habitaciones",
+    PropertyMeasurePText: "550 m2",
+    PropertyBathroomsPText: "3 baños"
+  },
+  {
+    urlImage1: 'url(./assets/homes/casa-5-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-5-2.jpg)',
+    urlImage3: 'url(./assets/homes/casa-5-3.jpg)',
+    stateHomeText: 'En Venta',
+    HomeTitleText:  'Altos de la chicha',
+    HomeSubTitleText: 'La palma, Bogotá',
+    PropertyNamePText:'Altos de la conejera',
+    PropertyBedroomsPText: "3 habitaciones",
+    PropertyMeasurePText: "550 m2",
+    PropertyBathroomsPText: "3 baños"
+  },
+  {
+    urlImage1: 'url(./assets/homes/casa-6-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-6-2.jpg)',
+    urlImage3: 'url(./assets/homes/casa-6-3.jpg)',
+    stateHomeText: 'En Arrendamiento',
+    HomeTitleText:  'Altos de la chicharita',
+    HomeSubTitleText: 'La plata, Bogotá',
+    PropertyNamePText:'Altos de la ezperanza',
+    PropertyBedroomsPText: "4 habitaciones",
+    PropertyMeasurePText: "410 m2",
+    PropertyBathroomsPText: "2 baños"
+  },
+  {
+    urlImage1: 'url(./assets/homes/casa-7-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-7-2.jpg)',
+    urlImage3: 'url(./assets/homes/casa-7-3.jpg)',
+    stateHomeText: 'En Arrendamiento',
+    HomeTitleText:  'Las lajas de la montaña',
+    HomeSubTitleText: 'Chapinero Alto, Bogotá',
+    PropertyNamePText:'Altos de la ezperanza',
+    PropertyBedroomsPText: "4 habitaciones",
+    PropertyMeasurePText: "410 m2",
+    PropertyBathroomsPText: "2 baños"
+  },
+  {
+    urlImage1: 'url(./assets/homes/casa-8-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-8-2.jpg)',
+    urlImage3: 'url(./assets/homes/casa-8-3.jpg)',
+    stateHomeText: 'En Venta',
+    HomeTitleText:  'Las lajas de la montaña',
+    HomeSubTitleText: 'Chapinero Bajo, Bogotá',
+    PropertyNamePText:'Altos de la estimación',
+    PropertyBedroomsPText: "4 habitaciones",
+    PropertyMeasurePText: "400 m2",
+    PropertyBathroomsPText: "1 baño"
+  },
+  {
+    urlImage1: 'url(./assets/homes/casa-9-1.jpg)',
+    urlImage2: 'url(./assets/homes/casa-9-2.jpg)',
+    urlImage3: 'url(./assets/homes/casa-9-3.jpg)',
+    stateHomeText: 'En Arrendamiento',
+    HomeTitleText:  'Las lajas de la montaña',
+    HomeSubTitleText: 'Chapinero Bajo, Bogotá',
+    PropertyNamePText:'Altos de la estimación',
+    PropertyBedroomsPText: "4 habitaciones",
+    PropertyMeasurePText: "400 m2",
+    PropertyBathroomsPText: "3 baños"
+  }
 ];
 
-const lastPropertiesContainer = document.querySelector('#last-properties-container');
-function Home(urlImage,stateHomeText,HomeTitleText,HomeSubTitleText,PropertyNamePText,PropertyBedroomsPText, PropertyMeasurePText, PropertyBathroomsPText){
-  const newHome = document.createElement('article'); 
-  newHome.className = 'last-property';
-  lastPropertiesContainer.appendChild(newHome);
+const lastPropertiesContainer = document.getElementById('last-properties-container');
 
-  const nabvarHomePhoto = document.createElement('div');
-  nabvarHomePhoto.className = "navbar-home-photo";
-  nabvarHomePhoto.style.backgroundImage =  urlImage;
+function lastProperty(
+  index,
+  salesState,
+  homeImageOneId,
+  homeImageTwoId,
+  homeImageThreeId,
+  lastPropertyTitle,
+  lastPropertySubTitle,
+  propertyNameP,
+  propertyBedroomsP,
+  propertyMeasureP,
+  propertyBathroomsP,
+  ){
+  this.index = index;
+  
+  this.salesState = salesState;
+  const psalesState = document.getElementById(salesState);
+  psalesState.innerHTML = Data[index].stateHomeText;
 
-  const stateHome = document.createElement('div');
-  stateHome.className = "state-home";
-  const stateHomeP = document.createElement('p');
-  stateHomeP.className = "p-small-bold p-small-state";
-  stateHomeP.innerHTML = stateHomeText;
-  stateHome.appendChild(stateHomeP);
+  this.homeImageOneId = homeImageOneId;
+  const homeImageOne =  document.getElementById(homeImageOneId);
+  homeImageOne.style.backgroundImage = Data[index].urlImage1;
 
-  const pointsNavbarHome = document.createElement('div');
-  pointsNavbarHome.className = 'points-nabvar-home';
-  const pointsItems = [];
-  for(let i=0;i<3;i++){
-    const pointNavbar =  document.createElement('span');
-    pointNavbar.className = 'point-nabvar-home';
-  pointsItems.push(pointNavbar); 
-  }
-  pointsNavbarHome.append(...pointsItems);
+  this.homeImageTwoId = homeImageTwoId;
+  const homeImageTwo = document.getElementById(homeImageTwoId);
+  homeImageTwo.style.backgroundImage = Data[index].urlImage2;
 
-  nabvarHomePhoto.append(stateHome,pointsNavbarHome);
+  this.homeImageThreeId = homeImageThreeId; 
+  const homeImageThree = document.getElementById(homeImageThreeId);
+  homeImageThree.style.backgroundImage = Data[index].urlImage3;
 
-  const HomeTitle = document.createElement('h3');
-  HomeTitle.className = 'h3-bold-title home-title';
-  HomeTitle.innerHTML = HomeTitleText;
+  this.lastPropertyTitle = lastPropertyTitle;
+  const propertyTitle = document.getElementById(lastPropertyTitle);
+  propertyTitle.innerHTML = Data[index].HomeTitleText;
 
-  const HomeSubtitle = document.createElement('p');
-  HomeSubtitle.className = 'p-medium-bold home-subtitle';
-  HomeSubtitle.innerHTML = HomeSubTitleText;
+  this.lastPropertySubTitle = lastPropertySubTitle;
+  const propertySubTitle = document.getElementById(lastPropertySubTitle);
+  propertySubTitle.innerHTML = Data[index].HomeSubTitleText;  
 
-  const LastPropertyDescription = document.createElement('div');
-  LastPropertyDescription.className = 'last-property-description';
+  this.propertyNameP = propertyNameP;
+  const propertyName = document.getElementById(propertyNameP);
+  propertyName.innerHTML = Data[index].PropertyNamePText;
 
-  const PropertyNameLogo = document.createElement('span');
-  PropertyNameLogo.className ="property-name-logo";
+  this.propertyBedroomsP = propertyBedroomsP;
+  const propertyBedrooms = document.getElementById(propertyBedroomsP);
+  propertyBedrooms.innerHTML = Data[index].PropertyBedroomsPText;
+  
+  this.propertyMeasureP = propertyMeasureP;
+  const propertyMeasure = document.getElementById(propertyMeasureP);
+  propertyMeasure.innerHTML = Data[index].PropertyMeasurePText; 
 
-  const PropertyNameP = document.createElement('p');
-  PropertyNameP.className = "p-medium-regular property-name-p";
-  PropertyNameP.innerHTML = PropertyNamePText;
-
-  const PropertyBedroomsLogo = document.createElement('span');
-  PropertyBedroomsLogo.className = 'property-bedrooms-logo';
-
-  const PropertyBedroomsP = document.createElement('p');
-  PropertyBedroomsP.className = "p-medium-regular property-bedrooms-p";
-  PropertyBedroomsP.innerHTML = PropertyBedroomsPText;
-
-  const PropertyMeasureLogo = document.createElement('span');
-  PropertyMeasureLogo.className ="property-measure-logo";
-
-  const PropertyMeasureP = document.createElement('p');
-  PropertyMeasureP.className = "p-medium-regular property-measure-p"
-  PropertyMeasureP.innerHTML = PropertyMeasurePText;
-
-  const PropertyBathroomsLogo = document.createElement('span');
-  PropertyBathroomsLogo.className = "property-bathrooms-logo";
-
-  const PropertyBathroomsP = document.createElement('p');
-  PropertyBathroomsP.className ="p-medium-regular property-bathrooms-p";
-  PropertyBathroomsP.innerHTML = PropertyBathroomsPText;
-  LastPropertyDescription.append(PropertyNameLogo,PropertyNameP,PropertyBedroomsLogo,PropertyBedroomsP,PropertyMeasureLogo,PropertyMeasureP,PropertyBathroomsLogo,PropertyBathroomsP);
-
-  const ButtonActionCellPhone = document.createElement('a');
-  ButtonActionCellPhone.className = "p-medium-bold button-action-cellphone";
-  ButtonActionCellPhone.innerHTML = "Ver Inmueble";
-  ButtonActionCellPhone.href = "/casa-1";
-  newHome.append(nabvarHomePhoto,HomeTitle,HomeSubtitle,LastPropertyDescription,ButtonActionCellPhone);
+  this.propertyBathroomsP = propertyBathroomsP;
+  const propertyBathrooms = document.getElementById(propertyBathroomsP);
+  propertyBathrooms.innerHTML = Data[index].PropertyBathroomsPText;  
 }
 
-
-for(let i=0;i<=3;i++){
-  Home(
-    Data[i].
-    urlImage,
-    Data[i].stateHomeText,
-    Data[i].HomeTitleText,
-    Data[i].HomeSubTitleText,
-    Data[i].PropertyNamePText,
-    Data[i].PropertyBedroomsPText, 
-    Data[i].PropertyMeasurePText, 
-    Data[i].PropertyBathroomsPText
-  )
-}
+const casa1 = new lastProperty(0,'p-sales-state-1','home-1-image-1','home-1-image-2','home-1-image-3','last-properties-title-1','last-properties-subtitle-1','property-name-p-1','property-bedrooms-p-1','property-measure-p-1','property-bathrooms-p-1');
+const casa2 = new lastProperty(1,'p-sales-state-2','home-2-image-1','home-2-image-2','home-2-image-3','last-properties-title-2','last-properties-subtitle-2','property-name-p-2','property-bedrooms-p-2','property-measure-p-2','property-bathrooms-p-2');
+const casa3 = new lastProperty(2,'p-sales-state-3','home-3-image-1','home-3-image-2','home-3-image-3','last-properties-title-3','last-properties-subtitle-3','property-name-p-3','property-bedrooms-p-3','property-measure-p-3','property-bathrooms-p-3');
 
 
--- styles.js
+++ styles.js
@@ -8,3 +8,22 @@ function buttonBurguer(){
 
 const burguerButton = document.getElementById('hero-menu-burguer');
 burguerButton.onclick = ()=> buttonBurguer();
+
+/*funciones de scroll para los bobtones verdades de las fotos de las casas */
+function firstGreenButton(principalHome){
+  const principalHomeCarousel =document.getElementById(principalHome);
+  principalHomeCarousel.classList.replace('principal-home-carousel-1','principal-home-carousel');
+  principalHomeCarousel.classList.replace('principal-home-carousel-2','principal-home-carousel');   
+}
+
+function secondGreenButton(principalHome){
+  const principalHomeCarousel =document.getElementById(principalHome);
+   principalHomeCarousel.classList.replace('principal-home-carousel','principal-home-carousel-1');
+   principalHomeCarousel.classList.replace('principal-home-carousel-2','principal-home-carousel-1');   
+}
+
+function thirdGreenButton(principalHome){
+  const principalHomeCarousel = document.getElementById(principalHome);
+  principalHomeCarousel.classList.replace('principal-home-carousel-1','principal-home-carousel-2');   
+  principalHomeCarousel.classList.replace('principal-home-carousel','principal-home-carousel-2');
+}
