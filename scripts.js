67babbc8638412a5f34e2ed1f16442219426c61f
Juan Franco <jfrancoh5@gmail.com>
2022-09-09 17:19:33 -0500

    casa hasta la parte de descripcion

--- scripts.js
+++ scripts.js
@@ -9,7 +9,13 @@
     PropertyNamePText:'Sky Apartments de la Holla',
     PropertyBedroomsPText: "3 habitaciones",
     PropertyMeasurePText: "76 m2",
-    PropertyBathroomsPText: "3 baños"
+    PropertyBathroomsPText: "3 baños",
+    PropertyPrice: 800000000,
+    PropertyMonthlyRent: 6400000,
+    PropertyLongDescription: `Esta es una casa muy bonita que está muy bien ubicada, y lo está por la sencilla razón de que queda en un barrio muy bonito de la ciudad.
+
+    Además tiene muy buenos acabados y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`,
+    PropertyAdress: 'Carrera 93U 131 - 26',
   },
   {
     urlImage1: 'url(./assets/homes/casa-2-1.jpg)',
@@ -21,7 +27,12 @@
     PropertyNamePText:'Cerros de la palma',
     PropertyBedroomsPText: "2 habitaciones",
     PropertyMeasurePText: "120 m2",
-    PropertyBathroomsPText: "3 baños"
+    PropertyBathroomsPText: "3 baños",
+    PropertyPrice: 1000000000,
+    PropertyLongDescription: `Esta es una casa fue arreglada por el mismísimo Lord Farquad, está muy bien ubicada y valorada, con excelente acabados y muy bonita.
+
+    Además tiene muy buena ubicación y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`,
+    PropertyAdress: 'Carrera 105P 31 12',
   },
   {
     urlImage1: 'url(./assets/homes/casa-3-1.jpg)',
@@ -33,7 +44,13 @@
     PropertyNamePText:'El rincón de la patraña',
     PropertyBedroomsPText: "2 habitaciones",
     PropertyMeasurePText: "300 m2",
-    PropertyBathroomsPText: "2 baños"
+    PropertyBathroomsPText: "2 baños",
+    PropertyPrice: 1300000000,
+    PropertyMonthlyRent: 10400000,
+    PropertyLongDescription: `Esta es una casa fue propiedad de Jairo Alonso, está muy bien ubicada en el barrio las cruces, con excelente acabados y muy bonita.
+
+    Además tiene muy buena ubicación y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`,
+    PropertyAdress: 'Calle 20 36 45',
   },
   {
     urlImage1: 'url(./assets/homes/casa-4-1.jpg)',
@@ -45,7 +62,12 @@
     PropertyNamePText:'Altos de la conejera',
     PropertyBedroomsPText: "3 habitaciones",
     PropertyMeasurePText: "550 m2",
-    PropertyBathroomsPText: "3 baños"
+    PropertyBathroomsPText: "3 baños",
+    PropertyPrice: 500000000,
+    PropertyLongDescription: `Esta es una casa fue arreglada por el mismísimo Shrek, está muy bien ubicada en el Gloria Lara, con excelente acabados y muy bonita.
+
+    Además tiene muy buena ubicación y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`,
+    PropertyAdress: 'Diagonal 123 12 - 88',
   },
   {
     urlImage1: 'url(./assets/homes/casa-5-1.jpg)',
@@ -57,7 +79,11 @@
     PropertyNamePText:'Altos de la conejera',
     PropertyBedroomsPText: "3 habitaciones",
     PropertyMeasurePText: "550 m2",
-    PropertyBathroomsPText: "3 baños"
+    PropertyBathroomsPText: "3 baños",
+    PropertyPrice: 500000000,
+    PropertyLongDescription: `Esta es una casa fue arreglada por Joseph Joestar, Después de su viaje por Egipto para vencer a Dio, la maldad pura.
+
+    Además tiene muy buena ubicación y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`
   },
   {
     urlImage1: 'url(./assets/homes/casa-6-1.jpg)',
@@ -69,7 +95,13 @@
     PropertyNamePText:'Altos de la ezperanza',
     PropertyBedroomsPText: "4 habitaciones",
     PropertyMeasurePText: "410 m2",
-    PropertyBathroomsPText: "2 baños"
+    PropertyBathroomsPText: "2 baños",
+    PropertyPrice: 125000000,
+    PropertyMonthlyRent: 1000000,
+    PropertyLongDescription: `Esta es una casa Bointa y bien educada, fue diseñada por la experta Arquitecta y lider en el sector Jocelyn Kujo, quien sabe mucho.
+
+    Además tiene muy buena ubicación y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`,
+    PropertyAdress: 'Calle 128 11 - 22',
   },
   {
     urlImage1: 'url(./assets/homes/casa-7-1.jpg)',
@@ -81,7 +113,13 @@
     PropertyNamePText:'Altos de la ezperanza',
     PropertyBedroomsPText: "4 habitaciones",
     PropertyMeasurePText: "410 m2",
-    PropertyBathroomsPText: "2 baños"
+    PropertyBathroomsPText: "2 baños",
+    PropertyPrice: 400000000,
+    PropertyMonthlyRent: 3200000,
+    PropertyLongDescription: `Esta es una casa diseñada con los más exquisitos detalles, fue creada por el grandísimo Jonhatan Joestar, y está muy bien ubicada.
+
+    Además tiene muy buena ubicación y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`,
+    PropertyAdress: 'Carrera 93U 131 - 26',
   },
   {
     urlImage1: 'url(./assets/homes/casa-8-1.jpg)',
@@ -93,7 +131,12 @@
     PropertyNamePText:'Altos de la estimación',
     PropertyBedroomsPText: "4 habitaciones",
     PropertyMeasurePText: "400 m2",
-    PropertyBathroomsPText: "1 baño"
+    PropertyBathroomsPText: "1 baño",
+    PropertyPrice: 900000000,
+    PropertyLongDescription: `Esta es una casa económica a comparación de las otras casas en el mercado, además tengamos en cuenta que es extremadamente comercial.
+
+    Además tiene muy buena ubicación y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`,
+    PropertyAdress: 'Calle 80 23a - 26',
   },
   {
     urlImage1: 'url(./assets/homes/casa-9-1.jpg)',
@@ -105,7 +148,13 @@
     PropertyNamePText:'Altos de la estimación',
     PropertyBedroomsPText: "4 habitaciones",
     PropertyMeasurePText: "400 m2",
-    PropertyBathroomsPText: "3 baños"
+    PropertyBathroomsPText: "3 baños",
+    PropertyPrice: 1150000000,
+    PropertyMonthlyRent: 9200000,
+    PropertyLongDescription: `Esta casita tiene inspraciones egipcias, y fue creada por el toque arquitectonico del gran navegador inglés, Dio Brando.
+
+    Además tiene muy buena ubicación y toda la cosa, y espero que vean esta casa para que vean el trabajo del desarrollador web, que merece ser contratado`,
+    PropertyAdress: 'Carrera 91 33 - 13',
   }
 ];
 
@@ -170,5 +219,7 @@ function lastProperty(
 const casa1 = new lastProperty(0,'p-sales-state-1','home-1-image-1','home-1-image-2','home-1-image-3','last-properties-title-1','last-properties-subtitle-1','property-name-p-1','property-bedrooms-p-1','property-measure-p-1','property-bathrooms-p-1');
 const casa2 = new lastProperty(1,'p-sales-state-2','home-2-image-1','home-2-image-2','home-2-image-3','last-properties-title-2','last-properties-subtitle-2','property-name-p-2','property-bedrooms-p-2','property-measure-p-2','property-bathrooms-p-2');
 const casa3 = new lastProperty(2,'p-sales-state-3','home-3-image-1','home-3-image-2','home-3-image-3','last-properties-title-3','last-properties-subtitle-3','property-name-p-3','property-bedrooms-p-3','property-measure-p-3','property-bathrooms-p-3');
+
+
 
 
